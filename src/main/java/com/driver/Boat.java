4ddff8663e506e651b0568c841e9345f786aaaef
Dev <kanuj2151@gmail.com>
2023-11-01 22:05:54 +0530

    1.0

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,2 +1,17 @@
-package com.driver;public class Boat {
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private  String name = "Titanic 2.0";
+    private int capacity = 5000;
+    public String getVehicleName() {
+        return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+
+
+
 }
