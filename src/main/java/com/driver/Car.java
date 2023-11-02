ea04cb01033c0a89dc75ab8b3e8ebef12c99fd59
Dev <kanuj2151@gmail.com>
2023-11-02 12:20:04 +0530

    recheked

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,6 +9,34 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,18 +2,18 @@ package com.driver;
 
 public class F1 extends Car {
 
-    private String name;
-    private boolean isManual;
+//    private String name;
+//    private boolean isManual;
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 4, 6, true, "F1", 5);
+        super(name, 4, 4, 6, isManual, "F1", 5);
         //this.name = name;
-        this.isManual = isManual;
+        //this.isManual = isManual;
 
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = rate + getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -23,22 +23,22 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        newSpeed += (rate + getCurrentSpeed());
+
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
             changeGear(1);
         }
         //for all other cases, change the gear accordingly
-        if (newSpeed > 1 && newSpeed  < 51) {
+        if (newSpeed  < 51) {
             changeGear(1);
-        } else if (newSpeed > 51 && newSpeed < 101) {
+        } else if (newSpeed < 101) {
             changeGear(2);
-        } else if (newSpeed > 100 && newSpeed < 151) {
+        } else if (newSpeed < 151) {
             changeGear(3);
-        } else if (newSpeed > 150 && newSpeed < 201) {
+        } else if (newSpeed < 201) {
             changeGear(4);
-        } else if(newSpeed > 200 && newSpeed < 251){
+        } else if(newSpeed < 251){
             changeGear(5);
         }
         else{
